0af6e85b8359bf70a15402b58d9425e7b2a9784b
devilboy0001 <141001196+devilboy0001@users.noreply.github.com>
2023-09-22 15:27:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29696092"))
 API_HASH = environ.get("API_HASH", "58d9ba5b26ff3b50fbb811d64c077541")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6439163306:AAGswAOd5WCrrlrmVj5jPKScfaUr_P8KWcI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6503758212:AAFR_g8oWQCSTUlm3E-kWART1z4bvVYJ23E")
 ADMIN = int(environ.get("ADMIN", "5263032279"))          
 CAPTION = environ.get("CAPTION", "main channel - https://t.me/devilboy_edit")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "https://graph.org/file/f19770f9bf165a3a5b489.jpg")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://graph.org/file/f64cd2728c3d57d244bad.jpg")
