0e9e78781bb9cf8506b2e82bd0cce1b354cfc6f8
devilboy0001 <141001196+devilboy0001@users.noreply.github.com>
2023-10-01 07:47:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29696092"))
 API_HASH = environ.get("API_HASH", "58d9ba5b26ff3b50fbb811d64c077541")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6503758212:AAFR_g8oWQCSTUlm3E-kWART1z4bvVYJ23E")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6349196945:AAFOAo44j_y68OPYm3rL9VUfaY66Bc7MXfs")
 ADMIN = int(environ.get("ADMIN", "5263032279"))          
 CAPTION = environ.get("CAPTION", "main channel - https://t.me/devilboy_edit")
 
